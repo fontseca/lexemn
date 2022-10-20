ca371e409f4715e1e84f72962dfa53ac5f6f1fcf
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-10-20 17:51:46 -0600

    include/lexer.h: Renamed to `include/analyzers/lexical-analyzer.h'.
      * include/lexer.h: Renamed to `include/analyzers/lexical-analyzer.h'.
      * include/parser.h: Renamed to `include/analyzers/syntactic-analyzer.h'

--- include/analyzers/syntactic-analyzer.h
+++ include/analyzers/syntactic-analyzer.h
@@ -1,5 +1,5 @@
 /*
- * lexer.h -- the lexical analyzer
+ * syntactic-analyzer.h -- the syntax analyzer
  *  ___       _______      ___    ___ _______   _____ ______   ________
  * |\  \     |\  ___ \    |\  \  /  /|\  ___ \ |\   _ \  _   \|\   ___  \
  * \ \  \    \ \   __/|   \ \  \/  / | \   __/|\ \  \\\__\ \  \ \  \\ \  \
@@ -29,40 +29,10 @@
  * Lexemn. If not, see <https://www.gnu.org/licenses/>.
  **/
 
-#ifndef LEXER_H
-#define LEXER_H
+#ifndef PARSER_H
+#define PARSER_H
 
-/*
- * The lexical analyzer takes a raw string as its input and will convert it
- * into a sequence of lexical tokens. These tokes are strings with an assigned
- * and identified value. The parser will use these tokens to later create the
- * parse tree.
- *
- * For the lexemn string expression
- *  "x:=1*2;",
- * the lexer will have to generate a structure of tokens similar to the
- * following:
- *   (lexeme,   token name)
- *   ("x",      <identifier>)
- *   ("1",      <literal>)
- *   ("*",      <operator>)
- *   ("2",      <literal>)
- *   (";",      <separator>).
- *
- * The lexer does not know where each token should be located, it can report
- * about not valid tokens (e.g., an illegal or unrecognized symbol) and other
- * malformed entities. It will not look for tokens out of space, mispelled
- * keywords, mistmatched types, etc.
- *
- * For the sentence '6 := f(x)', the lexer will not generate any error becasue
- * it has no concept of the appropiate arrangement of the tokens. It is the
- * parser's job to catch this error.
- *
- * The lexer can be a convenient place to carry out some other stuff like
- * stripping out comments and white spaces between tokens.
- */
-
-class lexer
+class parser
 {
 };
 
