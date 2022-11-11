0cb6e459f564b9f7a5db1f3c20cff7e08977121a
Jeremy Fonseca <fonseca.dev@outlook.com>
2022-11-11 10:18:50 -0600

    src/utilities.cc: New file.
      * src/utilities.cc: New file.

---
+++ src/utilities.cc
@@ -0,0 +1,50 @@
+/*
+ * utilities.cc --
+ *  ___       _______      ___    ___ _______   _____ ______   ________
+ * |\  \     |\  ___ \    |\  \  /  /|\  ___ \ |\   _ \  _   \|\   ___  \
+ * \ \  \    \ \   __/|   \ \  \/  / | \   __/|\ \  \\\__\ \  \ \  \\ \  \
+ *  \ \  \    \ \  \_|/__  \ \    / / \ \  \_|/_\ \  \\|__| \  \ \  \\ \  \
+ *   \ \  \____\ \  \_|\ \  /     \/   \ \  \_|\ \ \  \    \ \  \ \  \\ \  \
+ *    \ \_______\ \_______\/  /\   \    \ \_______\ \__\    \ \__\ \__\\ \__\
+ *     \|_______|\|_______/__/ /\ __\    \|_______|\|__|     \|__|\|__| \|__|
+ *                        |__|/ \|__|
+ *
+ * The Lexemn Project
+ * https://github.com/fontseca/lexemn
+ *
+ * Copyright (C) 2022 by Jeremy Fonseca <fonseca.dev@outlook.com>
+ *
+ * This file is part of Lexemn.
+ *
+ * Lexemn is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Lexemn is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Lexemn. If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+#include "lexemn/utilities.h"
+
+namespace lexemn::utilities
+{
+  namespace regex
+  {
+    std::regex digit("^(([0-9]*)|(([0-9]*)\\.([0-9]*)))$");
+    std::regex arithmetic_operator("[\\^+\\-*\\/]");
+    std::regex identifier("[a-zA-Z]");
+  }
+
+  /* Represents the way that Lexem will be running. */
+
+  running_mode x {
+    .quiet { 0 },
+    .debug { 0 },
+    .color { 0 },
+  };
+}
